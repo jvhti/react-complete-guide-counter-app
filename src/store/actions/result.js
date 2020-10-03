47293f7740125ca193b12032e273ba9fd415ed29
jvhti <jvhti@hotmail.com>
2020-10-03 16:46:22 -0300

    Revert "Example of using getState in a thunk"
    This reverts commit a30a2d69

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -3,11 +3,9 @@ import {DELETE_RESULT, STORE_RESULT} from "./actionTypes";
 export const saveResult = (result) => ({type: STORE_RESULT, result});
 
 export const storeResult = (result) => {
-  return (dispatch, getState) => {
+  return (dispatch) => {
     // Simulate a call to a server or something
     setTimeout(() => {
-      const oldCounter = getState().ctr.counter;
-      console.log(oldCounter);
       dispatch(saveResult(result));
     }, 2000);
   };
