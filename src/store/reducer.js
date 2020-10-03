7d5cabaa210793542448afb88352fb290013c2fa
jvhti <jvhti@hotmail.com>
2020-10-02 22:11:59 -0300

    Change if-else to switch on the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,17 +3,19 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {...state, counter: state.counter + 1};
-  } else if (action.type === 'DECREMENT') {
-    return {...state, counter: state.counter - 1};
-  } else if (action.type === 'ADD') {
-    return {...state, counter: state.counter + action.value};
-  } else if (action.type === 'SUBTRACT') {
-    return {...state, counter: state.counter - action.value};
+  switch (action.type) {
+    case 'INCREMENT':
+      return {...state, counter: state.counter + 1};
+    case 'DECREMENT':
+      return {...state, counter: state.counter - 1};
+    case 'ADD':
+      return {...state, counter: state.counter + action.value};
+    case 'SUBTRACT':
+      return {...state, counter: state.counter - action.value};
+    default:
+      return state;
   }
 
-  return state;
 };
 
 export default reducer;
